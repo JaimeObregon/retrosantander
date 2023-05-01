ab9fe14f5ef8b83e33d9b1c0c310770d52065627
Jaime Gómez-Obregón <jaime@gomezobregon.com>
2023-05-01 18:52:18 +0200

    Carga la estructura de datos independizada de dos proyectos

---
+++ httpdocs/retrogipuzkoa/project.js
@@ -0,0 +1,35 @@
+const project = {
+  name: 'Retrogipuzkoa',
+  folder: 'retrogipuzkoa',
+  languages: ['es'],
+  themes: ['dark'],
+  title: {
+    es: 'Explora 15 210 imágenes históricas de la Colección Jesús Elósegui',
+  },
+  image: (id) =>
+    `https://retrogipuzkoa.s3.eu-south-2.amazonaws.com/jpeg/${id}.jpg`,
+  metadata: (id) =>
+    `https://retrogipuzkoa.s3.eu-south-2.amazonaws.com/metadata/${id}.json`,
+  index: (id) =>
+    `https://retrogipuzkoa.s3.eu-south-2.amazonaws.com/indices/${id}.json`,
+  galleries: [],
+  collections: ['jesus_elosegui'],
+  locations: [],
+  routes: [
+    {
+      pattern: /^\/(\?q=(?<query>.+))?$/,
+      exec: (app, groups) => {
+        const collection = project.collections[0]
+        const index = project.index(collection)
+
+        const main = document.querySelector('main')
+        main.innerHTML = `<rs-grid index="${index}"></rs-grid>`
+
+        app.$grid = main.querySelector('rs-grid')
+        app.title = groups.query
+      },
+    },
+  ],
+}
+
+export { project }
---
+++ httpdocs/retrosantander/project.js
@@ -0,0 +1,37 @@
+const project = {
+  name: 'Retrosantander',
+  folder: 'retrosantander',
+  languages: ['es'],
+  themes: ['dark'],
+  title: {
+    es: 'Explora 9506 imágenes históricas de Santander',
+  },
+  image: (id) =>
+    // `https://portal.ayto-santander.es/portalcdis/image/DownloadFileExposicion.do?id=${id}`,
+    // `https://retrosantander.s3.eu-south-2.amazonaws.com/xxxx/xxxxxxxxx_xxxxxxx/${id}.jpeg`,
+    'assets/images/unavailable.svg',
+  metadata: (id) =>
+    `https://retrosantander.s3.eu-south-2.amazonaws.com/metadata/${id}.json`,
+  index: (id) =>
+    `https://retrosantander.s3.eu-south-2.amazonaws.com/indices/${id}.json`,
+  galleries: [],
+  collections: ['cdis'],
+  locations: [],
+  routes: [
+    {
+      pattern: /^\/(\?q=(?<query>.+))?$/,
+      exec: (app, groups) => {
+        const collection = project.collections[0]
+        const index = project.index(collection)
+
+        const main = document.querySelector('main')
+        main.innerHTML = `<rs-grid index="${index}"></rs-grid>`
+
+        app.$grid = main.querySelector('rs-grid')
+        app.title = groups.query
+      },
+    },
+  ],
+}
+
+export { project }
