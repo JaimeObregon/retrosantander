25bd1f0af1119a8de0f8b6d6f9fdc47c4fe433f4
Jaime Gómez-Obregón <417226+JaimeObregon@users.noreply.github.com>
2025-01-30 12:24:08 +0100

    Evoluciona el proyecto

--- httpdocs/modules/icons.js
+++ httpdocs/modules/icons.js
@@ -36,12 +36,6 @@ export const annotation = `
     <path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
   </svg>`
 
-export const faceFrown = `
-  <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
-    <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
-  </svg>
-`
-
 export const councilLogo = `
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.02 -0.02 408.6 186.83">
     <path d="M92.52 72.89a18.48 18.48 0 0 1-14.76 13.04v15.2c5.08 1.27 6.2 4.6 13.98 5.07 8.1-.5 9.4-4.07 15.14-5.2V85.76a19 19 0 0 1-14.36-12.88" />
