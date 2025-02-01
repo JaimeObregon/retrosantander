153e92e9a45c2ed107afd71f108cecba7eedd504
Jaime Gómez-Obregón <417226+JaimeObregon@users.noreply.github.com>
2025-02-01 16:40:36 +0100

    Evoluciona el proyecto

---
+++ httpdocs/components/rs-notice.js
@@ -0,0 +1,30 @@
+import { app } from '../modules/app.js'
+import { MyElement } from '../modules/element.js'
+import { css, html } from '../modules/strings.js'
+
+class Notice extends MyElement {
+  static styles = css`
+    :host {
+      max-width: 70ch;
+      margin: 0 var(--gap);
+    }
+
+    :host(.hidden) {
+      display: none;
+    }
+  `
+
+  static html = html`<slot></slot>`
+
+  async onLanguagechange() {
+    const url = `notice.${app.language}.html`
+    const response = await fetch(url)
+    this.innerHTML = await response.text()
+  }
+
+  connectedCallback() {
+    window.addEventListener('languagechange', this.onLanguagechange.bind(this))
+  }
+}
+
+export { Notice }
