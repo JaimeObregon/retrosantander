ea89e4b5531231c423f50d929677fbc90316f42d
Jaime Gómez-Obregón <417226+JaimeObregon@users.noreply.github.com>
2025-05-25 19:21:05 +0200

    Evoluciona el proyecto

--- httpdocs/components/rs-icon.js
+++ httpdocs/components/rs-icon.js
@@ -78,14 +78,6 @@ const icons = {
     </svg>
   `,
 
-  homeModern: html`
-    <svg class="icon" viewBox="0 0 24 24">
-      <path
-        d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"
-      />
-    </svg>
-  `,
-
   rectangleStack: html`
     <svg class="icon" viewBox="0 0 24 24">
       <path
