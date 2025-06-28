2737b4bd66559262ee5b46f685c21b2f8aacf9ed
Jaime Gómez-Obregón <417226+JaimeObregon@users.noreply.github.com>
2025-06-28 15:26:08 +0200

    Evoluciona el proyecto

--- httpdocs/guregipuzkoa/components/rs-map.js
+++ httpdocs/guregipuzkoa/components/rs-map.js
@@ -1,6 +1,6 @@
 import { app } from '../../modules/app.js'
 import { MyElement } from '../../modules/element.js'
-import { html, css, slugize } from '../../modules/strings.js'
+import { css } from '../../modules/strings.js'
 
 class Map extends MyElement {
   static styles = css`
@@ -11,28 +11,23 @@ class Map extends MyElement {
       min-height: 45vmax;
       background: var(--color-map-sea);
 
-      g {
+      path {
+        fill: var(--color-map-land);
+        stroke: var(--color-map-lines);
+        stroke-linecap: round;
+        stroke-linejoin: round;
+        transition: fill var(--delay-x-large);
+      }
+
+      a {
         path {
-          fill: var(--color-map-land);
-          stroke: var(--color-map-lines);
-          stroke-linecap: round;
-          stroke-linejoin: round;
-          transition: fill var(--delay-x-large);
+          fill: var(--color-map-place);
+          stroke-width: 2;
         }
 
-        &[data-title] {
-          cursor: pointer;
-
-          path {
-            fill: var(--color-map-place);
-            stroke-width: 2;
-          }
-
-          &:hover path {
-            cursor: pointer;
-            fill: var(--color-accent);
-            transition: fill var(--delay-small);
-          }
+        &:hover path {
+          fill: var(--color-accent);
+          transition: fill var(--delay-small);
         }
       }
     }
@@ -54,48 +49,19 @@ class Map extends MyElement {
       return
     }
 
-    this.onClick = (event) => {
-      const isPath = event.target instanceof SVGPathElement
-      if (!isPath) {
-        return
-      }
-
-      const g = event.target.closest('g')
-
-      if (!g.dataset.title) {
-        return
-      }
-
-      const slug = slugize(g.dataset.title)
-      const path = `/ikusi/places/${slug}`
-
-      const url = new URL(path, document.location.href)
-
-      history.pushState(null, '', url.href)
-
-      app.dispatch(path)
-    }
-
     this.onMouseover = (event) => {
-      const isPath = event.target instanceof SVGPathElement
-      if (!isPath) {
+      const link = event.target.closest('a[title]')
+      if (!link) {
         return
       }
 
-      const g = event.target.closest('g')
-
-      if (!g.dataset.title) {
-        return
-      }
-
-      app.title = event.target.closest('g')?.dataset.title
+      app.title = link.getAttribute('title')
     }
 
     this.onMouseout = () => {
       app.title = ''
     }
 
-    this.myAddEventListener(svg, 'click', this.onClick)
     this.myAddEventListener(svg, 'mouseout', this.onMouseout)
     this.myAddEventListener(svg, 'mouseover', this.onMouseover)
   }
