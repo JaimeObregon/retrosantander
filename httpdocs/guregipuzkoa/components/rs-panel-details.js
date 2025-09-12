60dc554f509201c174c559ff82fcbeb17af897bf
Jaime Gómez-Obregón <417226+JaimeObregon@users.noreply.github.com>
2025-09-12 20:03:09 +0200

    Evoluciona el proyecto

--- httpdocs/guregipuzkoa/components/rs-panel-details.js
+++ httpdocs/guregipuzkoa/components/rs-panel-details.js
@@ -64,14 +64,69 @@ class PanelDetails extends MyElement {
     </dl>
   `
 
+  whatsapp
+  download
+  link
+
+  buildWhatsAppLink(language) {
+    const { whatsapp } = app.project
+    const message = whatsapp.message(window.location.href)
+    const encodedMessage = encodeURIComponent(message[language])
+    const url = whatsapp.url(encodedMessage)
+    return url
+  }
+
+  connectedCallback() {
+    this.whatsapp = this.shadowRoot?.querySelector('a#whatsapp')
+    this.download = this.shadowRoot?.querySelector('a[download]')
+    this.link = this.shadowRoot?.querySelector('a#link')
+
+    i18n.push({
+      'panel.whatsapp': {
+        es: 'Enviar por WhatsApp',
+        eu: 'Bidali WhatsApp bidez',
+        en: 'Send via WhatsApp',
+        fr: 'Envoyer par WhatsApp',
+      },
+      'panel.download': {
+        es: 'Descargar imagen',
+        eu: 'Irudia deskargatu',
+        en: 'Download image',
+        fr: 'Télécharger l’image',
+      },
+      'panel.link': {
+        es: 'Ver #<code><slot name="id"></slot></code> en guregipuzkoa.eus',
+        eu: 'Ikusi #<code><slot name="id"></slot></code> guregipuzkoa.eus-en',
+        en: 'View #<code><slot name="id"></slot></code> on guregipuzkoa.eus',
+        fr: 'Voir #<code><slot name="id"></slot></code> sur guregipuzkoa.eus',
+      },
+    })
+
+    this.render()
+
+    this.onLanguagechange = () => this.render()
+
+    this.myAddEventListener(window, 'languagechange', this.onLanguagechange)
+  }
+
+  render() {
+    const language = i18n.getLanguage()
+
+    this.download.innerHTML = i18n.get('panel.download')
+    this.whatsapp.innerHTML = i18n.get('panel.whatsapp')
+    this.link.innerHTML = i18n.get('panel.link')
+
+    this.whatsapp.setAttribute('href', this.buildWhatsAppLink(language))
+  }
+
   set data(data) {
-    const { details } = data
+    this.innerHTML = html`
+      <span slot="id">${data.id}</span>
+      <span slot="caption">${data.json.summary.caption}</span>
+    `
 
-    const slots = Object.entries(details).map(
-      ([key, value]) => html`<span slot="${key}">${value}</span>`,
-    )
-
-    this.innerHTML = slots.join('')
+    this.download.setAttribute('href', app.project.image(data.id))
+    this.link.setAttribute('href', app.project.external(data.id))
   }
 }
 
